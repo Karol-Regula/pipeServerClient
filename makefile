c96453a756be06af011f4fdf17c54f4518d1cca3
Karol-Regula <kregula@stuy.edu>
2016-12-16 11:57:09 -0500

    Fixed several errors. Modified makefile.

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
-compile: pipe_networking.c client.c server.c pipe_networking.h
-    gcc pipe_networking.c -o pipe_networking; gcc client.c -o client; gcc server.c -o server
+compile: pipe_networking.c pipe_networking.h client.c server.c
+	gcc pipe_networking.c -o pipe_networking; gcc client.c -o client; gcc server.c -o server
 
 clean:
-    rm *~
+	rm *~
--- pipe_networking.c
+++ pipe_networking.c
@@ -9,23 +9,24 @@
 
 int server_handshake(int * from_client){
 	int status;
+	int * to_client;
 	char received[MESSAGE_BUFFER_SIZE];
 	char out[MESSAGE_BUFFER_SIZE];
 
 	status = mkfifo("mario", 0644);
-	from_client = open("mario", O_RDONLY, 0644);
+	*from_client = open("mario", O_RDONLY, 0644);
 
 	read(*from_client, received, MESSAGE_BUFFER_SIZE);
 	remove("mario");
 
 	//BREAK UP RIGHT HERE
 
-	to_client = open(received, O_WRONLY, 0644);
+	*to_client = open(received, O_WRONLY, 0644);
 
 	strcpy(out,"Hello");
-	write(to_client, out, MESSAGE_BUFFER_SIZE);
+	write(*to_client, out, MESSAGE_BUFFER_SIZE);
 
-	return to_client;
+	return *to_client;
 }
 
 int client_handshake(int * to_server){
@@ -33,20 +34,21 @@ int client_handshake(int * to_server){
 	char out[MESSAGE_BUFFER_SIZE];
 	char received[MESSAGE_BUFFER_SIZE];
 	char out2[MESSAGE_BUFFER_SIZE];
+	int * from_server;
 
 	sprintf(out, "%d" ,getpid());
 	status = mkfifo(out, 0644);
 
-	to_server = open("mario", O_WRONLY, 0644);
+	*to_server = open("mario", O_WRONLY, 0644);
 	write(*to_server, out, MESSAGE_BUFFER_SIZE);
 
-	from_server = open(out, O_RDONLY, 0644);
+	*from_server = open(out, O_RDONLY, 0644);
 	read(*from_server, received, MESSAGE_BUFFER_SIZE);
 
 	remove(out);
 	write(*to_server, out2, MESSAGE_BUFFER_SIZE);
 
-	return from_server;
+	return *from_server;
 }
 
 int main() {

08b566b19ed73121d920803fe371e927263c4c40
Noah Fichter <noahfichter@MacBook-Pro.local>
2016-12-16 11:44:45 -0500

    should work but clang error

--- pipe_networking.c
+++ pipe_networking.c
@@ -4,37 +4,51 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include "pipe_networking.h"
 
 int server_handshake(int * from_client){
 	int status;
-	int fdKnown;
-	int fdPrivate;
 	char received[MESSAGE_BUFFER_SIZE];
 	char out[MESSAGE_BUFFER_SIZE];
 
 	status = mkfifo("mario", 0644);
-	fdKnown = open("mario", O_RDONLY, 0644);
+	from_client = open("mario", O_RDONLY, 0644);
 
-	read(fdKnown, recieved, MESSAGE_BUFFER_SIZE);
+	read(*from_client, received, MESSAGE_BUFFER_SIZE);
 	remove("mario");
 
-	fdPrivate = open(*recieved, O_WRONLY, 0644);
+	//BREAK UP RIGHT HERE
 
+	to_client = open(received, O_WRONLY, 0644);
 
-	strcpy();
-	write(fdPrivate, );
+	strcpy(out,"Hello");
+	write(to_client, out, MESSAGE_BUFFER_SIZE);
+
+	return to_client;
 }
 
 int client_handshake(int * to_server){
-	char temp[MESSAGE_BUFFER_SIZE];
-	int fdKnown;
-	int fdPrivate;
-	sprintf(temp, "%d" ,getpid());
-	mkfifo(temp), 0644);
+	int status;
+	char out[MESSAGE_BUFFER_SIZE];
+	char received[MESSAGE_BUFFER_SIZE];
+	char out2[MESSAGE_BUFFER_SIZE];
 
-	fdKnown = open("mario", O_WRONLY, 0644);
-	write(fdKnown, temp, MESSAGE_BUFFER_SIZE;
+	sprintf(out, "%d" ,getpid());
+	status = mkfifo(out, 0644);
 
-	open(*temp, O_RDONLY, 0644);
+	to_server = open("mario", O_WRONLY, 0644);
+	write(*to_server, out, MESSAGE_BUFFER_SIZE);
 
+	from_server = open(out, O_RDONLY, 0644);
+	read(*from_server, received, MESSAGE_BUFFER_SIZE);
+
+	remove(out);
+	write(*to_server, out2, MESSAGE_BUFFER_SIZE);
+
+	return from_server;
+}
+
+int main() {
+	return 0;
 }
